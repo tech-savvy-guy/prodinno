62c18cbdf0e3821ae4ba752e3425d5e9b3dc4bdb
Soham Datta <dattasoham805@gmail.com>
2025-12-05 19:29:19 +0530

    `add analytics`

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -48,6 +49,7 @@ export default function RootLayout({
         className={`${geistSans.className} antialiased`}
       >
         {children}
+        <Analytics />
       </body>
     </html>
   );
